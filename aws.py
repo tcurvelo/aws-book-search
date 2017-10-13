35b62b1d85f3c5b79edcced21feed2c9f6ae82c9
Thiago Curvelo <tcurvelo@gmail.com>
2017-10-13 17:22:03 +0000

    Wrap aws access into a porper class

--- aws.py
+++ aws.py
@@ -10,52 +10,51 @@ import os
 import requests
 import sys
 
+class AmazonClient(object):
+    def __init__(self):
+        try:
+            keys = ('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_ASSOCIATE_TAG')
+            aws_ids = {key: os.environ[key] for key in keys}
+        except KeyError:
+            print('You should set the environment variables for accessing Amazon')
+            sys.exit(-1)
 
-def init():
-    try:
-        keys = ('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_ASSOCIATE_TAG')
-        aws_ids = {key: os.environ[key] for key in keys}
-    except KeyError:
-        print('You should set the environment variables for accessing Amazon')
-        sys.exit(-1)
+        self.amazon = bottlenose.Amazon(
+            aws_ids['AWS_ACCESS_KEY_ID'],
+            aws_ids['AWS_SECRET_ACCESS_KEY'],
+            aws_ids['AWS_ASSOCIATE_TAG'],
+            Parser=lambda text: BeautifulSoup(text, 'xml')
+        )
+        return amazon
 
-    amazon = bottlenose.Amazon(
-        aws_ids['AWS_ACCESS_KEY_ID'],
-        aws_ids['AWS_SECRET_ACCESS_KEY'],
-        aws_ids['AWS_ASSOCIATE_TAG'],
-        Parser=lambda text: BeautifulSoup(text, 'xml')
-    )
-    return amazon
+    def lookup(self, id):
+        result = self.amazon.ItemLookup(
+            ItemId=id, ResponseGroup="Large",
+            SearchIndex="Books", IdType="ISBN")
+        return _get_item_data(result)
 
-def lookup(amazon, id):
-    return amazon.ItemLookup(
-        ItemId=id, ResponseGroup="Large",
-        SearchIndex="Books", IdType="ISBN")
+    def _get_item_data(search_result):
+        reviews_url = search_result.find('IFrameURL').text
+        reviews_page = requests.get(reviews_url).text
 
-def get_item_data(result):
-    reviews_url = result.find('IFrameURL').text
-    reviews_page = requests.get(reviews_url).text
+        reviews = BeautifulSoup(reviews_page, 'html.parser')
+        attributes = search_result.find('ItemAttributes')
 
-    reviews = BeautifulSoup(reviews_page, 'html.parser')
-    attributes = result.find('ItemAttributes')
+        return {
+            'title': attributes.find('Title').text,
+            'author': attributes.find('Author').text,
+            'publisher': attributes.find('Manufacturer').text,
+            'release': attributes.find('ReleaseDate').text,
+            'rating': reviews.find_all('img')[1]['title'].split()[0],
+            'reviews': reviews.find('b').text.split()[0]
+        }
 
-    item = {
-        'title': attributes.find('Title').text,
-        'author': attributes.find('Author').text,
-        'publisher': attributes.find('Manufacturer').text,
-        'release': attributes.find('ReleaseDate').text,
-        'rating': reviews.find_all('img')[1]['title'].split()[0],
-        'reviews': reviews.find('b').text.split()[0]
-    }
-    return item
 
 def find(id=None):
     if id:
-        client = init()
-        result = lookup(client, id)
-        item = get_item_data(result)
+        client = AmazonClient()
+        item = lookup(client, id)
         pprint(item)
-
 
 if __name__ == '__main__':
     find(sys.argv[1])
