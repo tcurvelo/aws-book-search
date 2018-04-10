dde5d7948cf889a5e27d9d4cb0884a8b297891b7
Thiago Curvelo <tcurvelo@gmail.com>
2018-04-09 22:45:26 -0300

    Turn AWS client into a Class

--- aws.py
+++ aws.py
@@ -7,16 +7,19 @@ from pprint import pprint
 
 import bottlenose
 import os
-import requests
+# import requests
 import sys
+
 
 class AmazonClient(object):
     def __init__(self):
         try:
-            keys = ('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_ASSOCIATE_TAG')
+            keys = ('AWS_ACCESS_KEY_ID',
+                    'AWS_SECRET_ACCESS_KEY',
+                    'AWS_ASSOCIATE_TAG')
             aws_ids = {key: os.environ[key] for key in keys}
         except KeyError:
-            print('You should set the environment variables for accessing Amazon')
+            print('You should set the environment variables for accessing AWS')
             sys.exit(-1)
 
         self.amazon = bottlenose.Amazon(
@@ -25,36 +28,36 @@ class AmazonClient(object):
             aws_ids['AWS_ASSOCIATE_TAG'],
             Parser=lambda text: BeautifulSoup(text, 'xml')
         )
-        return amazon
 
     def lookup(self, id):
         result = self.amazon.ItemLookup(
             ItemId=id, ResponseGroup="Large",
             SearchIndex="Books", IdType="ISBN")
-        return _get_item_data(result)
+        return self._get_item_data(result)
 
-    def _get_item_data(search_result):
-        reviews_url = search_result.find('IFrameURL').text
-        reviews_page = requests.get(reviews_url).text
+    def _get_item_data(self, search_result):
+        # reviews_url = search_result.find('IFrameURL').text
+        # reviews_page = requests.get(reviews_url).text
 
-        reviews = BeautifulSoup(reviews_page, 'html.parser')
+        # reviews = BeautifulSoup(reviews_page, 'html.parser')
         attributes = search_result.find('ItemAttributes')
 
         return {
             'title': attributes.find('Title').text,
             'author': attributes.find('Author').text,
             'publisher': attributes.find('Manufacturer').text,
-            'release': attributes.find('ReleaseDate').text,
-            'rating': reviews.find_all('img')[1]['title'].split()[0],
-            'reviews': reviews.find('b').text.split()[0]
+            # 'release': attributes.find('ReleaseDate').text,
+            # 'rating': reviews.find_all('img')[1]['title'].split()[0],
+            # 'reviews': reviews.find('b').text.split()[0]
         }
 
 
 def find(id=None):
     if id:
         client = AmazonClient()
-        item = lookup(client, id)
+        item = client.lookup(id)
         pprint(item)
+
 
 if __name__ == '__main__':
     find(sys.argv[1])
--- epub.py
+++ epub.py
@@ -3,12 +3,14 @@ from ebooklib import epub
 
 import sys
 
+
 def get_isbn(filename):
     try:
         book = epub.read_epub(filename)
         return book.metadata['http://purl.org/dc/elements/1.1/']['identifier'][0][0]
     except Exception as error:
         print(error, sys.stderr)
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
