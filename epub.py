2686b05b229508e38185e84cf8b58b6c7e6ff01e
Thiago Curvelo <tcurvelo@gmail.com>
2023-06-10 15:49:30 -0300

    auto formating

--- epub.py
+++ epub.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-from ebooklib import epub
-
 import sys
+
+from ebooklib import epub
 
 
 def get_isbn(filename):
     try:
         book = epub.read_epub(filename)
-        return book.metadata['http://purl.org/dc/elements/1.1/']['identifier'][0][0]
+        return book.metadata["http://purl.org/dc/elements/1.1/"]["identifier"][0][0]
     except Exception as error:
         print(error, sys.stderr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     filename = sys.argv[1]
     print(get_isbn(filename))
